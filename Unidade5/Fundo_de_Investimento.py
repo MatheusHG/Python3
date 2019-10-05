ed7e8d0c659839621e44a4b4b40aebd57b63cbdd
MatheusHG <56003521+MatheusHG@users.noreply.github.com>
2019-10-05 19:28:19 -0300

    Questões do TST

--- Unidade5/Fundo_de_Investimento.py
+++ Unidade5/Fundo_de_Investimento.py
@@ -2,9 +2,10 @@
 #119210656
 #-------------------------------------
 #Fundo de Investimento - Unidade 5
-#   Nesse fundo de investimento simplificado, novas contribuções serã o aceitas desde que a quantia passada não seja menor 
-#que a média dos valores já investidos. Seu programa deve ler da entrada padrão uma sequência de valores a serem incluídos 
-#no fundo de investimento. Caso seja lida uma quantia que não obedece as regras acima, seu programa deve finalizar imprimindo 
+# Programa que gerencia operações em um fundo de investimento. Nesse fundo de investimento simplificado, 
+#novas contribuções serã o aceitas desde que a quantia passada não seja menor que a média dos valores já investidos. 
+#O programa ler da entrada padrão uma sequência de valores a serem incluídos no fundo de investimento. 
+#Caso seja lida uma quantia que não obedece as regras acima, seu programa deve finalizar imprimindo 
 #o valor total do fundo de investimento e a média das contribuições. Obs: Não considerar a última quantia.
     
 quant_valor = 0
